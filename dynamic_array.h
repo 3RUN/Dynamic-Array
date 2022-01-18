d4eede858e0fb9227d2b250ca01038b6c7920522
Magomet Kochkarov <parkour09@mail.ru>
2022-01-18 21:05:49 +0300

    babushka fix

--- dynamic_array.h
+++ dynamic_array.h
@@ -176,7 +176,7 @@ void *_array_get_element_at(array_t *array, size_t index, size_t type_size);
  * Ends the enumerating through the array.
  * \param   a           Pointer to the array we are getting element from.
  */
-#define array_enumerate_end(a) } } int babushka_n_balalayka
+#define array_enumerate_end(a) } } do {} while(0)
 
 #include "dynamic_array.c"
 #endif

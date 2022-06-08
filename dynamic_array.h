db0810665f5560121ce7651266e7db31289a652a
Magomet Kochkarov <parkour09@mail.ru>
2022-06-08 13:39:18 +0300

    Update dynamic_array.h

--- dynamic_array.h
+++ dynamic_array.h
@@ -145,7 +145,7 @@ void *_array_get_element_at(array_t *array, size_t index, size_t type_size);
  * Removes all elements from the given array.
  * \param   a           Pointer to the array we are removing all elements from.
  */
-#define array_clear(a) do { int i, count = a->count; for(i = 0; i < count; i ++) array_remove_last(t, a); }while(0)
+#define array_clear(a) do { int i, count = a->count; for(i = 0; i < count; i ++) array_remove_last(var, a); }while(0)
 
 /**
  * Returns the last element from the array. The element will be casted to the given data type.
